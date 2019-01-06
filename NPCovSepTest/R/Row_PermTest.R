a62a5307258603934ad218a31473fc50e385ef9e
inmybrain <seongohpark6@gmail.com>
2019-01-06 23:40:11 +0900

    first-try

--- NPCovSepTest/R/Row_PermTest.R
+++ NPCovSepTest/R/Row_PermTest.R
@@ -15,13 +15,13 @@ Row_PermTest <-
       idx_i <- (q * (pair[1] - 1) + 1):(q * pair[1])
       idx_j <- (q * (pair[2] - 1) + 1):(q * pair[2])
       Y_ij <- Y_rescale[,c(idx_i, idx_j)] 
-      lrt_ij <- getLRT(Y_ij, q, 2, Mean = 0)$lrt
+      lrt_ij <- getLRT(Y_ij, q, 2, Mean = matrix(0, nrow = nrow(Y_ij), ncol = ncol(Y_ij)))$lrt
       suppressWarnings(
         Perm_lrt <- foreach(noarg = 1:N_perm, .combine = "c", 
                             .export = c("PermY", "getLRT", "Y_ij", "q")) %dorng%
                             {
                               PY_ij <- PermY(Y_ij)
-                              return(getLRT(PY_ij, q, 2, Mean = 0)$lrt)
+                              return(getLRT(PY_ij, q, 2, Mean = matrix(0, nrow = nrow(PY_ij), ncol = ncol(PY_ij)))$lrt)
                             }
       )
       Perm_pvalue <- mean(unlist(Perm_lrt) > lrt_ij)
